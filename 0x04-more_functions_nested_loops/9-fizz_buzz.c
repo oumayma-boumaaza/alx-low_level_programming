c598d487cf7d496aec56000de5b7ffc6932aca83
oumayma-boumaaza <oumayma.boumaaza@usmba.ac.ma>
2023-04-27 08:18:36 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - primary function with for loop
+ *
+ * Description: for loop with if/else to print digits from 1 to 100
+ * Return: na
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x < 101; x++)
+	{
+		if (x % 3 == 0)
+		{
+			if (x % 5 == 0)
+				printf("FizzBuzz ");
+			else
+				printf("Fizz ");
+		} else if (x % 5 == 0)
+		{
+			if (x == 100)
+				printf("Buzz\n");
+			else
+				printf("Buzz ");
+		} else
+			printf("%d ", x);
+	}
+	return (0);
+}
