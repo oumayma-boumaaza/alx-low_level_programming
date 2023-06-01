5d311f00837fb1d4b93c1b4e3124d764a59cd986
Oumayma boumaaza <63117044+oumayma-boumaaza@users.noreply.github.com>
2023-06-01 22:52:27 +0100

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,32 +11,31 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *new_node;
 	list_t *temp = *head;
-	unsigned int len = 0;
+	unsigned int str_len = 0;
 
-	while (str[len])
-		len++;
+	while (str[str_len])
+		str_len++;
 
-	new = malloc(sizeof(list_t));
-	if (!new)
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
 		return (NULL);
 
-	new->str = strdup(str);
-	new->len = len;
-	new->next = NULL;
+	new_node->str = strdup(str);
+	new_node->len = str_len;
+	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
-		*head = new;
-		return (new);
+		*head = new_node;
+		return (new_node);
 	}
 
 	while (temp->next)
 		temp = temp->next;
 
-	temp->next = new;
+	temp->next = new_node;
 
-	return (new);
+	return (new_node);
 }
-
