8cd1829cb3dde45ea3cd5a72a92a403ef5beda9d
oumayma-boumaaza <oumayma.boumaaza@usmba.ac.ma>
2023-06-08 22:39:55 +0000

    frst push

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,25 @@
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: Pointer to a string of 0 and 1 chars.
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * Return: The converted number, or 0 if there is an invalid char or b is NULL.
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
+	int count;
 	unsigned int result = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	while (*b)
+	for (count = 0; b[count]; count++)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[count] < '0' || b[count] > '1')
 			return (0);
-
-		result <<= 1;
-		result += (*b - '0');
-
-		b++;
+		result = 2 * result + (b[count] - '0');
 	}
 
 	return (result);
 }
-
