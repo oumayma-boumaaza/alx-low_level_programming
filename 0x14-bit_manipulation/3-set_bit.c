1bcb68e2839d87d8ed26d1b466238542cd70d911
oumayma-boumaaza <oumayma.boumaaza@usmba.ac.ma>
2023-06-08 22:51:26 +0000

    4th push

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - sets a bit at a given index to 1
+ * @j: pointer to the number to change
+ * @i: index of the bit to set to 1
+ *
+ * Return: 1 for success, -1 for failure
+ */
+int set_bit(unsigned long int *j, unsigned int i)
+{
+	if (i > 63)
+		return (-1);
+
+	*j = ((1UL << i) | *j);
+	return (1);
+}
