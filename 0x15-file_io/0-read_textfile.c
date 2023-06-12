22366aa98afbc171930d1afb003e886702390154
oumayma-boumaaza <oumayma.boumaaza@usmba.ac.ma>
2023-06-12 23:15:00 +0100

    frst push

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,24 +5,25 @@
  * read_textfile- Read text file print to STDOUT.
  * @filename: text file being read
  * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
+ * Return: actual number of bytes read and printed (w)
  *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	ssize_t file_descriptor;
+	ssize_t bytes_read;
+	ssize_t total_bytes_read;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	total_bytes_read = read(file_descriptor, buf, letters);
+	bytes_read = write(STDOUT_FILENO, buf, total_bytes_read);
 
 	free(buf);
-	close(fd);
-	return (w);
+	close(file_descriptor);
+	return (bytes_read);
 }
+
